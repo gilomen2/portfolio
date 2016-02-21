186f42157d360259e05962a65d1ee648950b0312
Beth Gilomen <gilomen2@gmail.com>
2016-02-21 17:58:12 -0600

    fixes for heroku

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module Portfolio
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.config.serve_static_files = true
   end
 end
